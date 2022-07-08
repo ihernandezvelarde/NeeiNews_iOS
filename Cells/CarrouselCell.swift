088129a7f412d5bf256162dcdd1b4e08675d5729
ihernandezvelarde <neeiihernandez@gmail.com>
2022-07-08 17:14:18 +0200

    //Add constraints and tab bar on MainStoryBoard. //Add CarrouselCell class and CarrouselCell desgin. // Add view, collection view and var's on MainViewController.

---
+++ Cells/CarrouselCell.swift
@@ -0,0 +1,21 @@
+//
+//  CarrouselCell.swift
+//  NewsApp_iOS
+//
+//  Created by Irene Hernández on 8/7/22.
+//
+
+import UIKit
+
+class CarrouselCell: UICollectionViewCell {
+
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    @IBOutlet weak var newsFirstLineLabel: UILabel!
+    @IBOutlet weak var newsSecondLineLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- NewsApp_iOS/MainViewController.swift
+++ NewsApp_iOS/MainViewController.swift
@@ -9,13 +9,14 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    var newsTitle: [String] = ["Title 1","Title 2","Title 3","Title 4","Title 5","Title 6","Title 7"]
+    var newsFirstLine = "This is the first line, this is the first line"
+    var newsSecondLine = "This is the second line, this is the second line"
+    @IBOutlet weak var collectionView: UICollectionView!
     let launchImage = UIImageView(image: UIImage(named: "news_icon")!)
     let splashView = UIView()
-         
+    
     @IBOutlet weak var simpleView: UIView!
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,21 @@ class MainViewController: UIViewController {
     
         // FIXME: -- Change background color when design it's done.
         simpleView.backgroundColor = .green
-
     }
 }
+//extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+//    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return newsTitle.count
+//    }
+//    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CarrouselCell", for: indexPath) as! CarrouselCell
+//        cell.newsTitleLabel.text = newsTitle[indexPath.row]
+//        cell.newsFirstLineLabel.text = newsFirstLine
+//        cell.newsSecondLineLabel.text = newsSecondLine
+//        return cell
+//    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+//    }
+//
+//}
