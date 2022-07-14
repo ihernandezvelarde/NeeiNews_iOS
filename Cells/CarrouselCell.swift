81ad336a6f58944ee15602de09147aeeafcd39f9
ihernandezvelarde <neeiihernandez@gmail.com>
2022-07-14 16:07:10 +0200

    //Add constraints on MainStoryBoard. // Add Timer on CarrouselCell.

--- Cells/CarrouselCell.swift
+++ Cells/CarrouselCell.swift
@@ -9,26 +9,49 @@ import UIKit
 
 class CarrouselCell: UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource{
     @IBOutlet weak var carrouselCollectionView: UICollectionView!
-    
+    var currentCellIndex = 0
+    var newsTitle: [String] = ["Title 1","Title 2","Title 3","Title 4","Title 5","Title 6","Title 7"]
+    var newsFirstLine = "This is the first line"
+    var newsSecondLine = "This is the second line"
+    var timer : Timer?
+
     public override func awakeFromNib() {
         super.awakeFromNib()
         carrouselCollectionView.delegate = self
         carrouselCollectionView.dataSource = self
+        timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(slideToNext), userInfo: nil, repeats: true)
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        self.carrouselCollectionView.collectionViewLayout = layout
+    }
+    @objc func slideToNext() {
+        if currentCellIndex < newsTitle.count-1 {
+            currentCellIndex = currentCellIndex + 1
+        }else {
+            currentCellIndex = 0
+        }
+            carrouselCollectionView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .right, animated: true)
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        1
+        return newsTitle.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "innerCell", for: indexPath) as! celdita
-        //cell.newsTitleLabel.text = newsTitle[indexPath.row]
-        //cell.newsFirstLineLabel.text  = newsFirstLine
-        //cell.newsSecondLineLabel.text = newsSecondLine
+        cell.newsTitleLabel.text = newsTitle[indexPath.row]
+        cell.newsFirstLineLabel.text  = newsFirstLine
+        cell.newsSecondLineLabel.text = newsSecondLine
         return cell
     }
 
-////      timer = Timer.scheduledTimer(timeInterval: 8.0, target: NewsCarrousel.self, selector: #selector(slideToNext), userInfo: nil, repeats: true)
+}
+extension CarrouselCell: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 414, height: 128)
     }
+    
+////      timer = Timer.scheduledTimer(timeInterval: 8.0, target: NewsCarrousel.self, selector: #selector(slideToNext), userInfo: nil, repeats: true)
 //
 ////        @objc func slideToNext() {
 ////            if currentCellIndex < newsTitle.count-1 {
@@ -40,3 +63,4 @@ class CarrouselCell: UICollectionViewCell, UICollectionViewDelegate, UICollectio
 ////        }
     
 
+}
--- Cells/celdita.swift
+++ Cells/celdita.swift
@@ -11,4 +11,12 @@ class celdita: UICollectionViewCell {
     @IBOutlet weak var newsTitleLabel: UILabel!
     @IBOutlet weak var newsFirstLineLabel: UILabel!
     @IBOutlet weak var newsSecondLineLabel: UILabel!
+    
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        
+//        if newsTitleLabel != nil && newsFirstLineLabel != nil && newsSecondLineLabel != nil {
+//        newsTitleLabel.font = UIFont(name: "Title 1", size: 25)
+//        }
+    }
 }
