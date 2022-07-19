b77709d1f3584828e4023f82bb78ba5ae45462a1
ihernandezvelarde <neeiihernandez@gmail.com>
2022-07-19 17:06:38 +0200

    //Connect labels, images and buttons in MainStoryBoard to TimeLineViewController class. // Add labels, buttons and images on TimeLineViewClass.

--- NewsApp_iOS/TimeLineViewController.swift
+++ NewsApp_iOS/TimeLineViewController.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import UIKit
+class TimeLineViewController: UIViewController{
+
+    @IBOutlet weak var firsNewLabel: UILabel!
+    @IBOutlet weak var secondNewLabel: UILabel!
+    @IBOutlet weak var thirdNewLabel: UILabel!
+    @IBOutlet weak var forthNewLabel: UILabel!
+    @IBOutlet weak var fiveNewLabel: UILabel!
+    @IBOutlet weak var sixNewLabel: UILabel!
+    
+    
+    @IBOutlet weak var firstNewButton: UIButton!
+    
+    @IBOutlet weak var firstPartNextNewOneImage: UIImageView!
+    @IBOutlet weak var secondPartNextNewTwoImage: UIImageView!
+    
+    @IBOutlet weak var secondNewButton: UIButton!
+    
+    @IBOutlet weak var firstPartNextNewThreeImage: UIImageView!
+    @IBOutlet weak var secondPartNextNewFourImage: UIImageView!
+    
+    @IBOutlet weak var thirdNewButton: UIButton!
+    
+    @IBOutlet weak var firstPartNextNewFiveImage: UIImageView!
+    @IBOutlet weak var secondPartNextNewSixImage: UIImageView!
+    
+    @IBOutlet weak var fourthNewButton: UIButton!
+    
+    @IBOutlet weak var firstPartNextNewSevenImage: UIImageView!
+    @IBOutlet weak var secondPartNextNewEigthImage: UIImageView!
+    
+    @IBOutlet weak var fiveNewButton: UIButton!
+    
+    @IBOutlet weak var firstPartNextNewNineImage: UIImageView!
+    @IBOutlet weak var secondPartNextNewTenImage: UIImageView!
+    
+    @IBOutlet weak var sixNewButton: UIButton!
+    
+}
