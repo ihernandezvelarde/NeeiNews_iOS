97d2f83319d89c6b8c4037824de760c270aae8f6
ihernandezvelarde <neeiihernandez@gmail.com>
2022-07-21 12:08:44 +0200

    //Add tint color images, change label text, change checkTime function on TimeLineViewController.

--- NewsApp_iOS/TimeLineViewController.swift
+++ NewsApp_iOS/TimeLineViewController.swift
@@ -45,8 +45,10 @@ class TimeLineViewController: UIViewController{
     @IBOutlet weak var sixNewImage: UIImageView!
     
     var timer : Timer?
+    var prueba = "10:0:0"
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         firstNewImage.image = UIImage(named: "radio-button")
         secondNewImage.image = UIImage(named: "radio-button")
         thirdNewImage.image = UIImage(named: "radio-button")
@@ -55,87 +57,108 @@ class TimeLineViewController: UIViewController{
         sixNewImage.image = UIImage(named: "radio-button")
         
         firstPartNextNewOneImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        secondPartNextNewTwoImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        firstPartNextNewThreeImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        secondPartNextNewFourImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        firstPartNextNewFiveImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        secondPartNextNewSixImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        firstPartNextNewSevenImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        secondPartNextNewEigthImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        firstPartNextNewNineImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-        secondPartNextNewTenImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
-       
+        firstPartNextNewOneImage.tintColor = .black
         
-        firsNewLabel.text = "8:00 - Esta es la primera noticia. Bla bla bla bla bla, bla bla bla bla bla, blablabla blabla bla bla bla."
-        secondNewLabel.text = "9:00 - Esta es la segunda noticia. Bla bla bla bla bla, bla bla bla bla bla, blablabla blabla bla bla bla."
-        thirdNewLabel.text = "9:15 - Esta es la tercera noticia. Bla bla bla bla bla, bla bla bla bla bla, blablabla blabla bla bla bla."
-        forthNewLabel.text = "9:45 - Esta es la cuarta noticia. Bla bla bla bla bla, bla bla bla bla bla, blablabla blabla bla bla bla."
-        fiveNewLabel.text = "10:30 - Esta es la quinta noticia. Bla bla bla bla bla, bla bla bla bla bla, blablabla blabla bla bla bla."
-        sixNewLabel.text = "12:00 - Esta es la sexta noticia. Bla bla bla bla bla, bla bla bla bla bla, blablabla blabla bla bla bla."
+        secondPartNextNewTwoImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        secondPartNextNewTwoImage.tintColor = .black
+        
+        firstPartNextNewThreeImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        firstPartNextNewThreeImage.tintColor = .black
+        
+        secondPartNextNewFourImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        secondPartNextNewFourImage.tintColor = .black
+
+        firstPartNextNewFiveImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        firstPartNextNewFiveImage.tintColor = .black
+
+        secondPartNextNewSixImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        secondPartNextNewSixImage.tintColor = .black
+
+        firstPartNextNewSevenImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        firstPartNextNewSevenImage.tintColor = .black
+
+        secondPartNextNewEigthImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        secondPartNextNewEigthImage.tintColor = .black
+
+        firstPartNextNewNineImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        firstPartNextNewNineImage.tintColor = .black
+
+        secondPartNextNewTenImage.image = UIImage(named: "line")?.withRenderingMode(.alwaysTemplate);
+        secondPartNextNewTenImage.tintColor = .black
+
+        
+        firsNewLabel.text = "8:00 - Esta es la primera noticia. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nunc convallis."
+        secondNewLabel.text = "9:00 - Esta es la segunda noticia. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nunc convallis."
+        thirdNewLabel.text = "9:15 - Esta es la tercera noticia. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nunc convallis."
+        forthNewLabel.text = "9:45 - Esta es la cuarta noticia. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nunc convallis."
+        fiveNewLabel.text = "10:30 - Esta es la quinta noticia. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nunc convallis."
+        sixNewLabel.text = "12:00 - Esta es la sexta noticia. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nunc convallis."
+        
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(checkTime), userInfo: nil, repeats: true)
     }
     @objc func checkTime(){
         let date = Date()
         let calendar = Calendar.current
-
         let hour = calendar.component(.hour, from: date)
         let minutes = calendar.component(.minute, from: date)
         let seconds = calendar.component(.second, from: date)
-        print("hours = \(hour):\(minutes):\(seconds)")
         
-        if hour >= 8 && minutes >= 0 {
+        print("hours = \(hour):\(minutes):\(seconds)")
+        let hours = ("\(hour):\(minutes):\(seconds)")
+        
+        if hours >= "8:0:0" {
             firstNewImage.image = UIImage(named: "radio-on-button")
             firstNewImage.tintColor = .red
         }
-        if hour >= 8 && minutes >= 25 {
+        if hours >= "8:25:0" {
             firstPartNextNewOneImage.tintColor = .red
         }
-        if hour >= 8 && minutes >= 45 {
+        if hours >= "8:45:0" {
             secondPartNextNewTwoImage.tintColor = .red
         }
-//        if hours >= "9:00:00"{
-//            secondNewImage.image = UIImage(named: "radio-on-button")
-//            secondNewImage.tintColor = .red
-//        }
-//        if hours >= "9:5:00"{
-//            firstPartNextNewThreeImage.tintColor = .red
-//        }
-//        if hours >= "9:10:00"{
-//            secondPartNextNewFourImage.tintColor = .red
-//        }
-//        if hours >= "9:15:00"{
-//            thirdNewImage.image = UIImage(named: "radio-on-button")
-//            thirdNewImage.tintColor = .red
-//        }
-//        if hours >= "9:25:00"{
-//            firstPartNextNewFiveImage.tintColor = .red
-//        }
-//        if hours >= "9:35:00"{
-//            secondPartNextNewSixImage.tintColor = .red
-//        }
-//        if hours >= "9:45:00"{
-//            fourthNewImage.image = UIImage(named: "radio-on-button")
-//            fourthNewImage.tintColor = .red
-//        }
-//        if hours >= "10:00:00"{
-//            firstPartNextNewSevenImage.tintColor = .red
-//        }
-//        if hours >= "10:15:00"{
-//            secondPartNextNewEigthImage.tintColor = .red
-//        }
-//        if hours >= "10:30:00"{
-//            fiveNewImage.image = UIImage(named: "radio-on-button")
-//            fiveNewImage.tintColor = .red
-//        }
-//        if hours >= "11:00:00"{
-//            firstPartNextNewNineImage.tintColor = UIColor.red
-//        }
-//        if hours >= "11:30:00"{
-//            secondPartNextNewTenImage.tintColor = UIColor.red
-//        }
-//        if hours >= "12:00:00"{
-//            sixNewImage.image = UIImage(named: "radio-on-button")
-//        }
+        if hours >= "9:0:0" {
+            secondNewImage.image = UIImage(named: "radio-on-button")
+            secondNewImage.tintColor = .red
+        }
+        if hours >= "9:05:0" {
+            firstPartNextNewThreeImage.tintColor = .red
+        }
+        if hours >= "9:10:0" {
+            secondPartNextNewFourImage.tintColor = .red
+        }
+        if hours >= "9:15:0" {
+            thirdNewImage.image = UIImage(named: "radio-on-button")
+            thirdNewImage.tintColor = .red
+        }
+        if hours >= "9:25:0" {
+            firstPartNextNewFiveImage.tintColor = .red
+        }
+        if hours >= "9:35:0" {
+            secondPartNextNewSixImage.tintColor = .red
+        }
+        if hours >= "9:45:0" {
+            fourthNewImage.image = UIImage(named: "radio-on-button")
+            fourthNewImage.tintColor = .red
+        }
+        if hours >= "10:0:0" {
+            firstPartNextNewSevenImage.tintColor = .red
+        }
+        if hours >= "10:25:0" {
+            secondPartNextNewEigthImage.tintColor = .red
+        }
+        if hours >= "10:30:0" {
+            fiveNewImage.image = UIImage(named: "radio-on-button")
+            fiveNewImage.tintColor = .red
+        }
+        if hours >= "11:0:0" {
+            firstPartNextNewNineImage.tintColor = UIColor.red
+        }
+        if hours >= "11:30:0" {
+            secondPartNextNewTenImage.tintColor = UIColor.red
+        }
+        if hours >= "12:0:0" {
+            sixNewImage.image = UIImage(named: "radio-on-button")
+        }
     }
 }
 
