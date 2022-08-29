f786a5122b2ce1ef02d6b39b6fe7d7121f7280a3
ihernandezvelarde <irene.hernandezihv@gmail.com>
2022-08-29 17:19:10 +0200

    //Change mapView frame on MapsViewController class.

--- NewsApp_iOS/Views/MapsViewController.swift
+++ NewsApp_iOS/Views/MapsViewController.swift
@@ -19,7 +19,7 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(mapView)
-        mapView.frame = CGRect(x: 20, y: 50, width: view.frame.self.width-40, height: view.frame.size.height-220)
+        mapView.frame = CGRect(x: 0, y: 35, width: view.frame.self.width, height: view.frame.size.height)
         mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.3318, longitude: -122.0312), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         manager.delegate = self
         view.backgroundColor = UIColor(red: 213 / 255.0, green: 209 / 255.0, blue: 255 / 255.0, alpha: 1.0)
@@ -28,11 +28,11 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate {
     private func createButton(){
         let button = CLLocationButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
         button.backgroundColor = .systemPurple
-        button.cornerRadius = 110
-        button.label = .currentLocation
+        button.cornerRadius = 200
+        button.label = .none
+        button.frame = CGRect(x: 10, y: 10, width: 10, height: 10)
         button.icon = .arrowOutline
-        //button.cornerRadius = 12
-        button.center = CGPoint(x: view.center.x, y: view.frame.size.height-125)
+        button.center = CGPoint(x: view.center.x + 156, y: view.frame.size.height - 125)
         view.addSubview(button)
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
@@ -41,7 +41,7 @@ class MapsViewController: UIViewController, CLLocationManagerDelegate {
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else { return }
-        self.manager.stopUpdatingLocation()
+        //self.manager.stopUpdatingLocation()
         mapView.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)), animated: true)
     }
 }
